f1dd3ccd57eaf03cebf8cdc7201886a07d456ecc
DeniArif <deniarifwibowo82@gmail.com>
2023-03-24 11:29:38 +0700

    perbaikan

--- insertionSort_188/insertionSort_188.cpp
+++ insertionSort_188/insertionSort_188.cpp
@@ -24,7 +24,7 @@ void input() {
 	cout << "======================" << endl;
 
 
-	for (int i = 0; 1 < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		cout << "Data ke-" << (i + 1) << ": ";
 		cin >> arr[i];
