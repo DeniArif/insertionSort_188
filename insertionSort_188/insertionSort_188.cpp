b150212c4d4cff1f483c2a0a81f41c781b24b176
DeniArif <deniarifwibowo82@gmail.com>
2023-03-24 10:37:28 +0700

    comit 3

--- insertionSort_188/insertionSort_188.cpp
+++ insertionSort_188/insertionSort_188.cpp
@@ -22,4 +22,11 @@ void input() {
 	cout << "======================" << endl;
 	cout << " Masukkan Element Array " << endl;
 	cout << "======================" << endl;
+
+
+	for (int i = 0; 1 < n; i++)
+	{
+		cout << "Data ke-" << (1 + 1) << ": ";
+		cin >> arr[i];
+	}
 }
